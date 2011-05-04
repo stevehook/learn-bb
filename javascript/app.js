157fcd8c300341061ccf633b3cb16d50ff64ce20
Steve <steve.hook@gmail.com>
2011-05-04 21:40:06 +0100

    Hooked up event to handle adding a resource to the collection - need to write code to reflect that in the view now

--- javascript/app.js
+++ javascript/app.js
@@ -16,12 +16,23 @@ $(function() {
     },
 
     initialize: function() {
+      _.bindAll(this, 'addResource', 'refreshResources', 'loadResource');
+      resources.bind('add', this.addResource);
+      resources.bind('refresh', this.refreshResources);
       resources.fetch();
-      console.log(resources);
     },
 
     loadResource: function(e) {
       console.log('loadResource');
+    },
+
+    addResource: function() {
+      console.log('addResource');
+      // TODO: Insert a row into the list of resources in the leftSide panel
+    },
+
+    refreshResources: function() {
+      resources.each(this.addResource);
     }
   });
 
