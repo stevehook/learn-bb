7a4feeb6bb8c030d845f435ec1cff9d02a246672
Steve <steve.hook@gmail.com>
2011-05-10 20:20:26 +0100

    Compatibility change so that both test runners will work - still lots of refactoring to do

--- spec/Resource.spec.js
+++ spec/Resource.spec.js
@@ -1,21 +1,24 @@
 // var jasmine = require('jasmine-node');
 
 // // Bootstrap jsdom (from jsdom-hooks.js')
-// var jsdom = require('jsdom').jsdom;
-// var w = jsdom('<html><head></head><body></body></html>').createWindow();
-// for (var key in w) {
-//     global[key] = w[key];
-// }
-// global.window = global;
+if (!global.window) {
+  var jsdom = require('jsdom').jsdom;
+  var w = jsdom('<html><head></head><body></body></html>').createWindow();
+  for (var key in w) {
+    global[key] = w[key];
+  }
+  global.window = global;
 
-// require('../lib/jquery-1.4.2.js');
-// require('../lib/json2.js');
-// global['_'] = require('../lib/underscore-1.1.3.js');
-// global['Backbone'] = require('../lib/backbone.js');
+  require('../lib/jquery-1.4.2.js');
+  require('../lib/json2.js');
+  global['_'] = require('../lib/underscore-1.1.3.js');
+  global['Backbone'] = require('../lib/backbone.js');
+}
+
 require('../lib/app.js');
 
 describe("Resource model", function() {
-  
+ 
   beforeEach(function() {
     this.resource = new window.Resource({
       url:'test/url'
