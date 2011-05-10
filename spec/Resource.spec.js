d24d32a9dc46593cc5a0ab7086f00c53cf3844e1
Steve <steve.hook@gmail.com>
2011-05-10 20:31:44 +0100

    Basic test now runs from command line 'node runner.js spec'

--- spec/Resource.spec.js
+++ spec/Resource.spec.js
@@ -1,34 +1,28 @@
 var jasmine = require('jasmine-node');
-var jsdom = require('jsdom');
-var window = jsdom.createWindow();
+var jsdom = require('jsdom').jsdom;
+var w = jsdom('<html><head></head><body></body></html>').createWindow();
 
-jsdom.env('<html></html>', [
-    '../lib/jquery-1.4.2.js',
-    '../lib/json2.js',
-    '../lib/underscore-1.1.3.js',
-    '../lib/backbone.js',
-    '../lib/app.js'
-  ], function(_errors, _window) {
-    console.log('callback from jsdom.env');
-    window = _window;
-    errors = _errors;
-  });
+for (var key in w) {
+    global[key] = w[key];
+}
 
-// require('../lib/jquery-1.4.2.js');
-// require('../lib/json2.js');
-// require('../lib/underscore-1.1.3.js');
-// require('../lib/backbone.js');
-// require('../lib/app.js');
+global.window = global;
+
+require('../lib/jquery-1.4.2.js');
+require('../lib/json2.js');
+global['_'] = require('../lib/underscore-1.1.3.js');
+global['Backbone'] = require('../lib/backbone.js');
+require('../lib/app.js');
 
 describe("Resource model", function() {
   
   beforeEach(function() {
-    this.resource = new Resource({
+    this.resource = new window.Resource({
       url:'test/url'
     });
     var collection = {
     };
-    this.todo.collection = collection;
+    this.resource.collection = collection;
   });
 
   describe("when instantiated", function() {
