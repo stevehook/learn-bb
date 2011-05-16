d1fed628764534892b841503666a2f80231f6098
Steve <steve.hook@gmail.com>
2011-05-16 21:38:54 +0100

    Added partial support

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ class App < Sinatra::Base
   set :static, true
   set :public, 'public'
 
+  helpers do
+    def partial(page, options={})
+      haml page, options.merge!(:layout => false)
+    end
+  end
+
   get '/' do
     haml :index
   end
