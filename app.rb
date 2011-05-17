188b881c6afedc80faa7d984cc48587792ae5b7f
Steve <steve.hook@gmail.com>
2011-05-17 21:07:07 +0100

    Added Ajax routes to app.rb and wired these up to the backbone client-side code - currently returning hard coded values

--- app.rb
+++ app.rb
@@ -14,5 +14,26 @@ class App < Sinatra::Base
     haml :index
   end
 
+  get '/resources' do
+    '[ {
+          "name": "First File",
+          "url": "data/first.json"
+        }, {
+          "name": "Second File",
+          "url": "data/second.json"
+        }, {
+          "name": "Third File",
+          "url": "data/third.json"
+        }
+      ]'
+  end
+
+  get '/resources/:name' do
+    '{
+      "name": "First",
+      "data": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }'
+  end
+
   run! if /app.rb$/ =~ $0
 end
