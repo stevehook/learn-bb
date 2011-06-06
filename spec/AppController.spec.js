b1d3baf7c21b819905163b7380e142f9e32e7305
Steve <steve.hook@gmail.com>
2011-06-06 20:18:50 +0100

    Added tests for controller routes - commented out for now because jsdom doesn't support location.hash etc.

---
+++ spec/AppController.spec.js
@@ -0,0 +1,27 @@
+// TODO: Finish writing this spec when JSDom supports the location.hash and the hashchange event
+// require('./initialise-jsdom.js');
+// require('../lib/controllers/appController.js');
+
+// window.location.hash = '';
+// var controller = new AppController();
+
+// describe("AppControllerRoutes", function() {
+
+//   beforeEach(function() {
+//     this.routeSpy = sinon.spy();
+//   });
+
+//   afterEach(function() {
+//     window.location.hash = "";
+//   });
+
+//   it("fires the index route with a blank hash", function() {
+//     controller.bind("route:index", this.routeSpy);
+//     window.location.hash = "";
+//     Backbone.history.start();
+//     expect(this.routeSpy).toHaveBeenCalledOnce();
+//     expect(this.routeSpy).toHaveBeenCalledWith();
+//     controller.unbind("route:index");
+//   });
+// });
+
--- spec/initialise-jsdom.js
+++ spec/initialise-jsdom.js
@@ -8,7 +8,7 @@ if (!global.window) {
 
   require('../lib/jquery-1.4.2.js');
   require('../lib/json2.js');
-  require('sinon');
+  global['sinon'] = require('sinon');
   global['_'] = require('../lib/underscore-1.1.3.js');
   global['Backbone'] = require('../lib/backbone.js');
 }
