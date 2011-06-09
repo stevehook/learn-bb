c02f45c56a49fd4325cee5faf4c64ed4c5a768c6
Steve <steve.hook@gmail.com>
2011-06-09 19:20:43 +0100

    Added dependency on sinon mocking framework (as a node package) so that we can start writing route (and other) specs

--- spec/initialise-jsdom.js
+++ spec/initialise-jsdom.js
@@ -8,6 +8,7 @@ if (!global.window) {
 
   require('../lib/jquery-1.4.2.js');
   require('../lib/json2.js');
+  require('sinon');
   global['_'] = require('../lib/underscore-1.1.3.js');
   global['Backbone'] = require('../lib/backbone.js');
 }
